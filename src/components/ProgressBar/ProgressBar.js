3d4a9d5f884c74dab48ff9dff3fd230215be87a3
almudenaredradix <almudena@redradix.com>
2023-08-24 11:23:37 +0200

    :lipstick: Fix some styles in order to improve border radius of the inner bar'

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,64 +5,79 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const SIZES = {
+const STYLES = {
   small: {
-    "--borderRadius": 4 + "px",
-    "--blockSize": 8 + "px",
+    height: 8,
+    padding: 0,
+    radius: 4,
   },
   medium: {
-    "--borderRadius": 4 + "px",
-    "--blockSize": 12 + "px",
+    height: 12,
+    padding: 0,
+    radius: 4,
   },
   large: {
-    "--borderRadius": 8 + "px",
-    "--blockSize": 24 + "px",
+    height: 16,
+    padding: 4,
+    radius: 8,
   }
 };
 
-const ProgressBarBase = styled.div`
-  background-color: ${COLORS.transparentGray15};
-  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  inline-size: 100%;
-  max-inline-size: 370px;
-  block-size: var(--blockSize);
-  position: relative;
-  border-radius: var(--borderRadius);
-  overflow: clip;
-`
 
-const Bar = styled.div`
-  position: absolute;
-  background-color: ${COLORS.primary};
-  block-size: 100%;
-  max-inline-size: calc(100% - 6px);
-  max-block-size: 16px;
-  margin-block-start: auto;
-  margin-block-end: auto;
-  inset-block-start: 0;
-  inset-block-end: 0;
-  inset-inline-start: 3px;
-  border-start-start-radius: var(--borderRadius);
-  border-start-end-radius: var(--borderRadius);
-`
 const ProgressBar = ({ value, size }) => {
-  const styles = SIZES[size]
+  const styles = STYLES[size]
 
-  const BarStyles = {
-    inlineSize: value + '%',
-    borderEndStartRadius: value === 100 && SIZES[size]['--borderRadius'],
-    borderEndEndRadius: value === 100 && SIZES[size]['--borderRadius'],
+  if(!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`)
   }
 
   return (
-    <ProgressBarBase role="progressbar" aria-labelledby="loadinglabel" aria-valuenow={value} style={styles}>
-      <Bar style={BarStyles} size={size}>
+    <Wrapper
+      role="progressbar"
+      aria-labelledby="loadinglabel"
+      aria-valuenow={value}
+      aria-valuemin='0'
+      aria-valuemax='100'
+      style={{
+        '--padding': styles.padding + 'px',
+        '--radius': styles.radius + 'px'
+      }}
+    >
+      <BarWrapper>
+        <Bar
+          style={{
+          '--width': value + '%',
+          '--height': styles.height + 'px'
+        }}
+          size={size}
+          >
+        </Bar>
+      </BarWrapper>
       <VisuallyHidden>
-        {value}
+        {value}%
       </VisuallyHidden>
-      </Bar>
-    </ProgressBarBase>
+    </Wrapper>
   )
 };
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--radius);
+  padding: var(--padding);
+`
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  /* Trim off corners when progress bar is near-full. */
+  overflow: hidden;
+`
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px ;
+`
 
 export default ProgressBar;
