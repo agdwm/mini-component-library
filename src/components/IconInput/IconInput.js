cc39bc41f11b5290e6cc374129cb4c4eb62439e6
almudenaredradix <almudena@redradix.com>
2023-08-27 18:09:58 +0200

    :lipstick: Replace the HTML in order to take advantage of the benefits of the label tag

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -9,28 +9,25 @@ import VisuallyHidden from '../VisuallyHidden';
 
 const STYLES = {
   small: {
-    'borderWidth': '1',
-    'fontSize': '14',
-    'iconSize': '16',
-    'minHeight': '24',
-    'paddingBlock': '4',
-    'paddingInline': '26',
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
   },
   large: {
-    'borderWidth': '2',
-    'fontSize': '18',
-    'iconSize': '24',
-    'minHeight': '36',
-    'paddingBlock': '8',
-    'paddingInline': '36',
-  }
-}
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
+  },
+};
 const IconInput = ({
   icon,
   label,
   placeholder,
   size,
   width = 250,
+  ...delegated
 }) => {
 
   const styles = STYLES[size]
@@ -40,87 +37,57 @@ const IconInput = ({
   }
 
   return (
-    <Wrapper style={{
-      '--width': width +'px',
-      '--min-height': styles.minHeight + 'px',
-      '--font-size': styles.fontSize + 'px',
-      '--border-width': styles.borderWidth + 'px',
-      }}>
-      <Input
-        type="text"
-        placeholder={placeholder}
-        style={{
-          '--padding-inline':styles.paddingInline + 'px',
-          '--padding-block': styles.paddingBlock + 'px',
-        }}/>
-      <VisuallyHidden>
-        <label>{label}</label>
-      </VisuallyHidden>
-      <IconWrapper style={{
-        '--size': 24 + 'px'
-      }}>
-        <Icon
-          id={icon}
-          strokeWidth={2}
-          size={styles.iconSize}
-          role="presentation"
-          aria-hidden="true"
-        />
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ '--size': styles.iconSize + 'px' }}>
+        <Icon id={icon} size={styles.iconSize} strokeWidth={2}/>
       </IconWrapper>
+      <TextInput
+        {...delegated}
+        style={{
+          '--width': width + 'px',
+          '--height': styles.height / 16 + 'rem',
+          '--border-thickness': styles.borderThickness + 'px',
+          '--font-size': styles.fontSize / 16 + 'rem',
+        }}
+      />
     </Wrapper>
-  )
+  );
 };
 
-const Input = styled.input`
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
   color: ${COLORS.gray700};
-  font-weight: 700;
-  height: 100%;
-  max-width: 100%;
-  padding-inline: var(--padding-inline);
-  padding-block: var(--padding-block);
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`;
+
+const TextInput = styled.input`
+  width: var(--width);
+  height: var(--height);
+  font-size: var(--font-size);
   border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
 
   &::placeholder {
     font-weight: 400;
     color: ${COLORS.gray500};
   }
-
-  &:focus {
-    outline: none;
-  }
-
-  &:hover {
-    color: ${COLORS.black};
-  }
-`
-
-const Wrapper = styled.div`
-  position: relative;
-  width: var(--width);
-  height: 100%;
-  min-height: var(--min-height);
-  font-size: var(--font-size);
-  border-bottom: var(--border-width) solid ${COLORS.black};
-  display: inline-block;
-
-  &:focus-within {
-    outline: 1px dotted #212121;
-    outline: 5px auto -webkit-focus-ring-color;
-    outline-offset: 2px;
-  }
-`
-
-const IconWrapper = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 2px;
-  transform: translateY(-50%);
-  color: ${COLORS.gray700};
-  pointer-events: none;
-
-  ${Input}:hover ~ & {
-    color: ${COLORS.black};
-  }
-`
+`;
 
 export default IconInput;
